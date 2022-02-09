697ca73b69c7a26c90ba78b6f1d9fd8a7d6c7b42
IbrahimHarcha <99190637+IbrahimHarcha@users.noreply.github.com>
2022-02-09 19:04:28 +0100

    Add files via upload

---
+++ test.py
@@ -0,0 +1,17 @@
+from tkinter import *
+
+# Creation et paramétrage de la fenêtre :
+app = Tk()
+app.title("Qui est-ce ?")
+app.geometry("800x620+400+100")
+app.minsize(800,620)
+
+# Widget :
+app.config(bg="black")
+lbl = Label(app, text="Qui est-ce ?",font=("Pristina", 40),bg="beige",height=2,width=20)
+bn = Button(app, text="Jouer",bg="beige",width=19, height=2, font=("Pristina", 25))
+
+lbl.pack(pady=200) 
+bn.pack()
+
+app.mainloop()
